a248f9331d2314d464155d652fa2521619e773de
Baozhu Zuo <zuobaozhu@gmail.com>
2020-02-02 13:02:15 +0800

    Pretty printed the Arduino code with astyle

--- src/util/DpsRegister.h
+++ src/util/DpsRegister.h
@@ -3,15 +3,13 @@
 
 #include <Arduino.h>
 
-typedef struct
-{
+typedef struct {
     uint8_t regAddress;
     uint8_t mask;
     uint8_t shift;
 } RegMask_t;
 
-typedef struct
-{
+typedef struct {
     uint8_t regAddress;
     uint8_t length;
 } RegBlock_t;
--- src/util/dps_config.h
+++ src/util/dps_config.h
@@ -62,69 +62,65 @@
 #define DPS__FAIL_TOOBUSY -3
 #define DPS__FAIL_UNFINISHED -4
 
-namespace dps
-{
+namespace dps {
 
-/**
- * @brief Operating mode.
- * 
- */
-enum Mode
-{
-    IDLE = 0x00,
-    CMD_PRS = 0x01,
-    CMD_TEMP = 0x02,
-    CMD_BOTH = 0x03, // only for DPS422
-    CONT_PRS = 0x05,
-    CONT_TMP = 0x06,
-    CONT_BOTH = 0x07
-};
+    /**
+        @brief Operating mode.
 
-enum RegisterBlocks_e
-{
-    PRS = 0, // pressure value
-    TEMP,    // temperature value
-};
+    */
+    enum Mode {
+        IDLE = 0x00,
+        CMD_PRS = 0x01,
+        CMD_TEMP = 0x02,
+        CMD_BOTH = 0x03, // only for DPS422
+        CONT_PRS = 0x05,
+        CONT_TMP = 0x06,
+        CONT_BOTH = 0x07
+    };
 
-const RegBlock_t registerBlocks[2] = {
-    {0x00, 3},
-    {0x03, 3},
-};
+    enum RegisterBlocks_e {
+        PRS = 0, // pressure value
+        TEMP,    // temperature value
+    };
 
-/**
- * @brief registers for configuration and flags; these are the same for both 310 and 422, might need to be adapted for future sensors
- * 
- */
-enum Config_Registers_e
-{
-    TEMP_MR = 0, // temperature measure rate
-    TEMP_OSR,    // temperature measurement resolution
-    PRS_MR,      // pressure measure rate
-    PRS_OSR,     // pressure measurement resolution
-    MSR_CTRL,    // measurement control
-    FIFO_EN,
+    const RegBlock_t registerBlocks[2] = {
+        {0x00, 3},
+        {0x03, 3},
+    };
 
-    TEMP_RDY,
-    PRS_RDY,
-    INT_FLAG_FIFO,
-    INT_FLAG_TEMP,
-    INT_FLAG_PRS,
-};
+    /**
+        @brief registers for configuration and flags; these are the same for both 310 and 422, might need to be adapted for future sensors
 
-const RegMask_t config_registers[NUM_OF_COMMON_REGMASKS] = {
-    {0x07, 0x70, 4}, // TEMP_MR
-    {0x07, 0x07, 0}, // TEMP_OSR
-    {0x06, 0x70, 4}, // PRS_MR
-    {0x06, 0x07, 0}, // PRS_OSR
-    {0x08, 0x07, 0}, // MSR_CTRL
-    {0x09, 0x02, 1}, // FIFO_EN
+    */
+    enum Config_Registers_e {
+        TEMP_MR = 0, // temperature measure rate
+        TEMP_OSR,    // temperature measurement resolution
+        PRS_MR,      // pressure measure rate
+        PRS_OSR,     // pressure measurement resolution
+        MSR_CTRL,    // measurement control
+        FIFO_EN,
 
-    {0x08, 0x20, 5}, // TEMP_RDY
-    {0x08, 0x10, 4}, // PRS_RDY
-    {0x0A, 0x04, 2}, // INT_FLAG_FIFO
-    {0x0A, 0x02, 1}, // INT_FLAG_TEMP
-    {0x0A, 0x01, 0}, // INT_FLAG_PRS
-};
+        TEMP_RDY,
+        PRS_RDY,
+        INT_FLAG_FIFO,
+        INT_FLAG_TEMP,
+        INT_FLAG_PRS,
+    };
+
+    const RegMask_t config_registers[NUM_OF_COMMON_REGMASKS] = {
+        {0x07, 0x70, 4}, // TEMP_MR
+        {0x07, 0x07, 0}, // TEMP_OSR
+        {0x06, 0x70, 4}, // PRS_MR
+        {0x06, 0x07, 0}, // PRS_OSR
+        {0x08, 0x07, 0}, // MSR_CTRL
+        {0x09, 0x02, 1}, // FIFO_EN
+
+        {0x08, 0x20, 5}, // TEMP_RDY
+        {0x08, 0x10, 4}, // PRS_RDY
+        {0x0A, 0x04, 2}, // INT_FLAG_FIFO
+        {0x0A, 0x02, 1}, // INT_FLAG_TEMP
+        {0x0A, 0x01, 0}, // INT_FLAG_PRS
+    };
 
 } // namespace dps
 #endif /* DPS_CONSTS_H_ */
